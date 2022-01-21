80c0101fed4ccfa25e21e1017f4a453583f8a102
Odelo <regan.col@northeastern.edu>
2022-01-20 19:45:33 -0500

    Initial Xcode App files Commit

---
+++ Capstone ROM App/Shared/Capstone_ROM_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Capstone_ROM_AppApp.swift
+//  Shared
+//
+//  Created by Colin Regan on 1/20/22.
+//
+
+import SwiftUI
+
+@main
+struct Capstone_ROM_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
